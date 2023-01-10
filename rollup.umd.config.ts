52098009cb01d9db92e0413ab1bd6fcbf715cb0f
caijf <369756941@qq.com>
2023-01-10 14:19:20 +0800

    feat: 添加构建配置

---
+++ rollup.umd.config.ts
@@ -0,0 +1,28 @@
+import type { RollupOptions } from 'rollup';
+import terser from '@rollup/plugin-terser';
+import { pkgName, outputFilePrefix, commonConfig } from './rollup.config';
+
+const globalVariableName = pkgName.charAt(0).toUpperCase() + pkgName.substring(1);
+
+const config: RollupOptions = {
+  ...commonConfig,
+  output: [
+    {
+      format: 'umd',
+      file: `${outputFilePrefix}.js`,
+      exports: 'default',
+      name: globalVariableName,
+      sourcemap: true,
+    },
+    {
+      format: 'umd',
+      file: `${outputFilePrefix}.min.js`,
+      exports: 'default',
+      name: globalVariableName,
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+};
+
+export default config;
