2272ed3353a96047b9570b6731918b3bcdc03dd2
caijf <369756941@qq.com>
2024-10-22 11:08:29 +0800

    chore: 提示调整

--- src/session.ts
+++ src/session.ts
@@ -2,7 +2,7 @@ import { inWindow } from './utils';
 import { Storage } from './Storage';
 
 /**
- * @deprecated 即将废弃，请使用 `const local = new Storage(window.sessionStorage)`
+ * @deprecated 即将废弃，请使用 `const session = new Storage(window.sessionStorage)`
  */
 const session = new Storage(inWindow ? window.sessionStorage : undefined);
 
