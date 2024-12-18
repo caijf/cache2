8c17a082b3482fa08116e9b592b467017700e146
caijf <369756941@qq.com>
2024-12-18 14:39:42 +0800

    build: 构建配置调整

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,11 +2,12 @@ import type { RollupOptions } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
-export const outputDir = 'dist';
-export const pkgName = 'cache2';
-export const outputFilePrefix = `${outputDir}/${pkgName}`;
-export const commonConfig = {
+const outputDir = 'dist';
+const pkgName = 'cache2';
+const outputFilePrefix = `${outputDir}/${pkgName}`;
+const commonConfig = {
   input: 'src/index.ts',
   plugins: [
     resolve(),
@@ -17,21 +18,43 @@ export const commonConfig = {
   ]
 };
 
-const config: RollupOptions = {
-  ...commonConfig,
-  output: [
-    {
-      format: 'cjs',
-      file: `${outputFilePrefix}.cjs.js`,
-      exports: 'named'
-    },
-    {
-      format: 'es',
-      file: `${outputFilePrefix}.esm.js`,
-      exports: 'named'
-    }
-  ],
-  external: ['tslib', 'emitter-pro']
-};
+const config: RollupOptions[] = [
+  {
+    ...commonConfig,
+    output: [
+      {
+        format: 'cjs',
+        file: `${outputFilePrefix}.cjs.js`,
+        exports: 'named'
+      },
+      {
+        format: 'es',
+        file: `${outputFilePrefix}.esm.js`,
+        exports: 'named'
+      }
+    ],
+    external: ['tslib', 'emitter-pro']
+  },
+  {
+    ...commonConfig,
+    output: [
+      {
+        format: 'umd',
+        file: `${outputFilePrefix}.js`,
+        exports: 'named',
+        name: pkgName,
+        sourcemap: true
+      },
+      {
+        format: 'umd',
+        file: `${outputFilePrefix}.min.js`,
+        exports: 'named',
+        name: pkgName,
+        sourcemap: true,
+        plugins: [terser()]
+      }
+    ]
+  }
+];
 
 export default config;
