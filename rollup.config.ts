30461a5214e82e6c5c882fcfee55512798ac39ae
caijf <369756941@qq.com>
2026-01-28 16:15:13 +0800

    build: 构建配置调整

--- rollup.config.ts
+++ rollup.config.ts
@@ -13,7 +13,8 @@ const commonConfig = {
     resolve(),
     commonjs(),
     typescript({
-      tsconfig: './tsconfig.build.json'
+      tsconfig: './tsconfig.build.json',
+      removeComments: true
     })
   ]
 };
